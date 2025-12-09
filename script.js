5418692b798dc3b6c6ddb9df6807756494112666
tako162 <154701396+tako162@users.noreply.github.com>
2025-12-09 17:29:47 +0900

    Revise seat management and drawing functions
    Refactor member count and seat drawing logic.

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
-const allMemberCount = 37;
 const memberNames = [];
+const allMemberCount = memberNames.length;
+const usedSeatNombers = [];
 let drawCount = 0;
-const usedSeats = [];
+
+function draw() {
+    drawCount++;
+    var random = Math.floor(Math.random() * allMemberCount + 1; 
+    var seat = documents.getElementById(`sear${random}`);
+}
 
 //座席を生成(CSS未連携)
 function addSeat(count) {
     var seats = document.getElementsByClassName("parent")[0];
     for (let i = 1; i < count + 1; i++) {
-        seats.insertAdjacentHTML("beforeend", `<div class="div${i}">`)
+        seats.insertAdjacentHTML("beforeend", `<div id="seat${i}" class="div${i}">`)
     }
 }
 addSeat(allMemberCount);
+
