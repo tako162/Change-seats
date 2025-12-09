7a091ad25ecc31875d78c63999af851e46f29382
tako162 <154701396+tako162@users.noreply.github.com>
2025-12-09 18:43:12 +0900

    Fix syntax errors and improve seat generation

--- script.js
+++ script.js
@@ -9,15 +9,16 @@ let drawCount = 0;
 
 function draw() {
     drawCount++;
-    var random = Math.floor(Math.random() * allMemberCount + 1; 
-    var seat = documents.getElementById(`sear${random}`);
+    var random = Math.floor(Math.random() * allMemberCount + 1); 
+    var seat = documents.getElementById(`seat${random}`);
 }
 
 //座席を生成(CSS未連携)
 function addSeat(count) {
     var seats = document.getElementsByClassName("parent")[0];
     for (let i = 1; i < count + 1; i++) {
-        seats.insertAdjacentHTML("beforeend", `<div id="seat${i}" class="div${i}">`);
+        seats.insertAdjacentHTML("beforeend", `<div id="seat${i}" class="div${i}"></div>`);
     }
 }
 addSeat(allMemberCount);
+
