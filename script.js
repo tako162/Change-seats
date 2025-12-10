c256f6c48f02eb837dfacea7d5469763c1a03511
tako162 <154701396+tako162@users.noreply.github.com>
2025-12-10 13:22:48 +0900

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ function draw() {
     drawCount++;
     var random = Math.floor(Math.random() * allMemberCount + 1); 
     var seat = document.getElementById(`seat${random}`);
-    document.getElementsByClassName("div" + seat)[0].innerText = `${random}`;;
+    document.getElementsByClassName(`div${random}`)[0].innerText = `${random}`;
     console.log(`seat${random}`);;
 }
 
@@ -22,6 +22,7 @@ function addSeat(count) {
     }
 }
 addSeat(allMemberCount);
+
 
 
 
